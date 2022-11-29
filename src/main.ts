26a9cd7a4e331539a59c2918bb16f5fa046fae08
rassi0429 <taitokokoa+rassi@gmail.com>
2022-11-30 07:53:20 +0900

    feat: remove loading

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import './samples/node-api'
 
 createApp(App)
   .mount('#app')
-  .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
-
 
 // Logger from Main Process
 window.api.on("log", (args: any) => {
